da285024a46aea863a50270d0441eb2cdb84491b
Xuan Tong <txmanialife@gmail.com>
2018-09-26 23:56:52 +1000

    add command

--- legit.pl
+++ legit.pl
@@ -8,7 +8,7 @@ Usage: legit.pl <command> [<args>]
 blah blah
 END_MESSAGE
 my $rootDirName = ".legit";
-my $legitStateFileName = ".legitStates";
+my $legitStateCurrentBranchFileName = ".legitStatesCurrentBranch";
 my $branchStateTrackedFilesIndexFileName = ".branchStatesTrackedFilesIndex";
 my $branchStateCommitNumberFileName = ".branchStatesCommitNumber";
 my $masterBranchName = "master";
@@ -23,11 +23,10 @@ if (@ARGV == 0) {
 if ($ARGV[0] eq "init") {
 	init();
 } elsif($ARGV[0] eq "add") {
-
+	add(@ARGV[1..@ARGV-1]);
 } else {
 	print "legit.pl: error: unknown command $ARGV[0]\n".$helpMessage;
 }
-
 
 ###### functions
 sub init {
@@ -42,9 +41,36 @@ sub init {
 		print "Initialized empty legit repository in $rootDirName\n";
 	}
 
-	system "touch $rootDirName/$legitStateFileName;
-		echo \"current_branch = master\" > $rootDirName/$legitStateFileName";
+	system "touch $rootDirName/$legitStateCurrentBranchFileName;
+		echo \"master\" > $rootDirName/$legitStateCurrentBranchFileName";
 	initBranch($masterBranchName);
+}
+
+sub add {
+	my @filesToAdd = @_;
+	checkIfRootDirExist();	
+
+	OUTER: foreach my $file (@filesToAdd) {
+		if (-e $file and -f $file) {
+			# check if it already exists in trackFileIndex
+			print "$rootDirName/".getCurrentBranch()."/$branchStateTrackedFilesIndexFileName";
+			open(my $fh, '<', "$rootDirName/".getCurrentBranch()."/$branchStateTrackedFilesIndexFileName");
+			INNER: while(my $line = <$fh>)  {   
+			    if ($line eq $file) {
+			    	print "File $file already added blah blah\n";
+			    	close($fh);
+			    	last OUTER;
+			    }
+			}
+			close($fh);
+			# append to .branchStatesTrackedFilesIndex,append
+			open(my $write_fh, '>>', "$rootDirName/".getCurrentBranch()."/$branchStateTrackedFilesIndexFileName");  
+			print $write_fh "$file\n";
+			close($write_fh);
+		} else {
+			print "$file does not valid blah blah\n";
+		}
+	}
 }
 
 
@@ -61,6 +87,20 @@ sub initBranch {
 	system "touch $rootDirName/$branchName/$branchStateTrackedFilesIndexFileName; 
 		touch $rootDirName/$branchName/$branchStateCommitNumberFileName;
 		echo \"current_commit = -1\" > $rootDirName/$branchName/$branchStateCommitNumberFileName;";
+}
+
+sub checkIfRootDirExist {
+	if (!(-e $rootDirName and -d $rootDirName)) {
+		die "$0: error: .legit does not exit, please init first blah blah\n";
+	}
+}
+
+sub getCurrentBranch {
+	open( FILE, "<$rootDirName/$legitStateCurrentBranchFileName" ); 
+    my @LINES = <FILE>; 
+    close(FILE);
+    chomp $LINES[0];
+    return $LINES[0];
 }
 
 # sub createCommit {
