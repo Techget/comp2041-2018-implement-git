c7215f5e7758369ba55dba2970c37295590febb1
Xuan Tong <txmanialife@gmail.com>
2018-09-27 23:13:56 +1000

    commit

--- legit.pl
+++ legit.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+# use Getopt::Std; # tested work on school machine
+use File::Copy qw(copy);
 
 ###### Universal variables
 my $helpMessage = <<'END_MESSAGE';
@@ -13,6 +15,7 @@ my $branchStateTrackedFilesIndexFileName = ".branchStatesTrackedFilesIndex";
 my $branchStateCommitNumberFileName = ".branchStatesCommitNumber";
 my $branchStateAddedFileFileName = ".branchStatesAddedFiles"; # ad hoc, only exists between add and commit
 my $masterBranchName = "master";
+my $commitMsgFileName = ".commitMsg";
 
 
 ###### helper functions
@@ -26,7 +29,7 @@ sub initBranch {
 
 	system "touch $rootDirName/$branchName/$branchStateTrackedFilesIndexFileName; 
 		touch $rootDirName/$branchName/$branchStateCommitNumberFileName;
-		echo \"current_commit = -1\" > $rootDirName/$branchName/$branchStateCommitNumberFileName;";
+		echo \"-1\" > $rootDirName/$branchName/$branchStateCommitNumberFileName;";
 }
 
 sub checkIfRootDirExist {
@@ -43,10 +46,78 @@ sub getCurrentBranch {
     return $LINES[0];
 }
 
-# sub createCommit {
+sub getCurrentCommitNumber {
+	open (FILE, '<', "$rootDirName/".getCurrentBranch()."/$branchStateCommitNumberFileName");
+	my $firstLine = <FILE>;
+	chomp $firstLine;
+	close FILE;
+	return $firstLine;
+}
 
-# }
+sub getCurrentCommitFolderPath {
+	return "$rootDirName/".getCurrentBranch()."/".getCurrentCommitNumber()."/";
+}
 
+sub getPreviousCommitFolderPath {
+	my $previousCommitNum = getCurrentBranch();
+	$previousCommitNum -= 1;
+	return "$rootDirName/".$previousCommitNum."/".getCurrentCommitNumber()."/";
+}
+
+sub getCurrentBranchAddedFilePath {
+	return "$rootDirName/".getCurrentBranch()."/$branchStateAddedFileFileName";
+}
+
+sub getCurrentBranchTrackedFilesIndexPath {
+	return "$rootDirName/".getCurrentBranch()."/$branchStateTrackedFilesIndexFileName";
+}
+
+sub getAllFilesListedInBranchTrackedFilesIndexFile {
+	open(my $fh, '<', getCurrentBranchTrackedFilesIndexPath()) 
+		or die ".trackedFilesIndex does not exists $!";
+	chomp(my @trackedFiles = <$fh>);
+	close $fh;
+	return @trackedFiles;
+}
+
+sub getAllFilesAddedToAddedFileTemporaryFile {
+	open(my $fh, '<', getCurrentBranchAddedFilePath()) 
+		or die ".BranchAddedFile does not exists $!";
+	chomp(my @addedFiles = <$fh>);
+	close $fh;
+	return @addedFiles;
+}
+
+sub increaseBranchCommitNumberByOne {
+	my $newCommitNum = 1 + getCurrentCommitNumber(); 
+	open (FILE, '>', "$rootDirName/".getCurrentBranch()."/$branchStateCommitNumberFileName") 
+		or die "CommitNumberFile does not exists $!";
+	print FILE $newCommitNum;
+	close FILE;
+}
+
+sub createAndInitNewCommitFolder {
+	my ($commitMsg) = @_;
+
+	# get next commit number
+	my $nextCommitNum = getCurrentCommitNumber() + 1;
+	# print "next commit number: $nextCommitNum\n";
+
+	# create new commit folder
+	my $return = mkdir "$rootDirName/".getCurrentBranch()."/$nextCommitNum";
+	if ($return != 1) {
+		die "Error creating new folder:".$!."\n";
+	}
+
+	# create new .commitMsg file and write commit msg
+	system "touch "."$rootDirName/".getCurrentBranch()."/$nextCommitNum/$commitMsgFileName";
+	open (FILE, ">", "$rootDirName/".getCurrentBranch()."/$nextCommitNum/$commitMsgFileName");
+	print FILE $commitMsg;
+	close FILE;
+
+	# increase commit nubmer
+	increaseBranchCommitNumberByOne();
+}
 
 
 ###### functions
@@ -71,34 +142,28 @@ sub add {
 	my @filesToAdd = @_;
 	checkIfRootDirExist();	
 
-	# Get already tracked files
-	open(my $fh, '<', "$rootDirName/".getCurrentBranch()."/$branchStateTrackedFilesIndexFileName");
-	chomp(my @trackedFiles = <$fh>);
-	close $fh;
+	# Get already added files
+	my @addedFiles = ();
 
-	if (-e "$rootDirName/".getCurrentBranch()."/$branchStateAddedFileFileName") {
-		open (my $fh, '<', "$rootDirName/".getCurrentBranch()."/$branchStateAddedFileFileName");
-		while (my $line = <$fh>) {
-			chomp $line;
-			push @trackedFiles, $line;
-		}
+	if (-e getCurrentBranchAddedFilePath()) {
+		@addedFiles = getAllFilesAddedToAddedFileTemporaryFile();
 	} else {
-		system "touch "."$rootDirName/".getCurrentBranch()."/$branchStateAddedFileFileName";
+		system "touch ".getCurrentBranchAddedFilePath();
 	}
-	my %trackedFilesHash = map { $_ => 1 } @trackedFiles;
+	my %addedFilesHash = map { $_ => 1 } @addedFiles;
 
 	# print "already exists files: \n";
-	# foreach (sort keys %trackedFilesHash) {
-	# 	print "$_ : $trackedFilesHash{$_}\n";
+	# foreach (sort keys %addedFilesHash) {
+	# 	print "$_ : $addedFilesHash{$_}\n";
 	# }
 
 	# prepare write to .branchStatesAddedFile
-	open(my $write_fh, '>>', "$rootDirName/".getCurrentBranch()."/$branchStateAddedFileFileName");
+	open(my $write_fh, '>>', getCurrentBranchAddedFilePath());
 
 	foreach my $file (@filesToAdd) {
 		if (-e $file and -f $file) {
-			# check if it already exists in trackedFilesHash			
-			if(!exists($trackedFilesHash{$file})) { 
+			# check if it already exists in addedFilesHash			
+			if(!exists($addedFilesHash{$file})) { 
 				# write to `addedFile` 
 				print $write_fh "$file\n";
 			}
@@ -110,6 +175,62 @@ sub add {
 	close($write_fh);
 }
 
+sub commit {
+	my ($commitMsg) = @_;
+	checkIfRootDirExist();	
+
+	if (! -e getCurrentBranchAddedFilePath()) {
+		print "Nothing to commit, not sure just create a new commit or abort current execution\n";
+		return;
+	}
+
+	# create the new commit directory, add commit message file
+	createAndInitNewCommitFolder($commitMsg);
+
+	# write addedFile to branchStatestrackedFilesIndex(committed tracking) if file not exists in trackingFile
+	## get files listed in trackingFileIndex and added files
+	my @trackedFiles = getAllFilesListedInBranchTrackedFilesIndexFile();
+	my %trackedFilesHash = map { $_ => 1 } @trackedFiles;
+	my @addedFiles = getAllFilesAddedToAddedFileTemporaryFile();
+	## append to trackingFileIndex is file exists in addedFile but not trackingFileIndex
+	open (my $write_fh, '>>', getCurrentBranchTrackedFilesIndexPath());
+	foreach my $addedFile (@addedFiles) {
+		if (!exists($trackedFilesHash{$addedFile})) {
+			print $write_fh "$addedFile\n";
+		}
+	}
+	close($write_fh);
+
+	# copy files in addedFileIndex from disk to newly created commit folder
+	my $currentCommitFolderPath = getCurrentCommitFolderPath();
+	foreach my $addedFile (@addedFiles) {
+		copy($addedFile, $currentCommitFolderPath.$addedFile);
+	}
+
+	# copy rest of tracked files from last commit folder 
+	my $previousCommitFolderPath = getPreviousCommitFolderPath();
+	if (-e $previousCommitFolderPath and -d $previousCommitFolderPath) { 
+		# get rest of tracked files
+		foreach my $addedFile (@addedFiles) {
+			if (exists $trackedFilesHash{$addedFile}) {
+				delete $trackedFilesHash{$addedFile}; 
+			}
+		}
+
+		foreach my $trackedFile (keys %trackedFilesHash) {
+			copy($previousCommitFolderPath.$trackedFile, $currentCommitFolderPath.$trackedFile);
+		}
+	}
+
+	# remove temporary addedFile index file
+	unlink getCurrentBranchAddedFilePath();
+
+	# print Info
+	print "Committed as commit ".getCurrentCommitNumber()."\n";
+}
+
+
+
 
 ###### main switch case
 if (@ARGV == 0) {
@@ -118,11 +239,46 @@ if (@ARGV == 0) {
 }
 
 if ($ARGV[0] eq "init") {
+	
 	init();
+
 } elsif($ARGV[0] eq "add") {
+
 	add(@ARGV[1..@ARGV-1]);
+
+} elsif($ARGV[0] eq "commit") {
+	# extract flags
+	my $flag_a_set = 0;
+	my $flag_m_set = 0;
+	my $commitMsg = 0;
+	foreach my $arg (@ARGV[1..@ARGV-1]) {
+		if ($arg =~ m/^\-/) {
+			if ($arg eq "-a") {
+				$flag_a_set = 1;
+			} elsif ($arg eq "-m") {
+				$flag_m_set = 1;
+			} else {
+				die "unknown commit flag blah blah\n";
+			}
+		} elsif ($flag_m_set) {
+			# make an assumption here, if -m set, the next msg do not start with - is commit msg
+			$commitMsg = $arg;
+		} else {
+			die "unknown commit option\n";
+		}
+	}
+
+	# do commit
+	if ($flag_a_set) {
+		
+	}
+	if ($flag_m_set) {
+		commit($commitMsg);
+	} else {
+		die "no commit message blah blah\n";
+	}
 } else {
-	print "legit.pl: error: unknown command $ARGV[0]\n".$helpMessage;
+	die "legit.pl: error: unknown command $ARGV[0]\n".$helpMessage;
 }
 
 
