7a07fa42c3942fde868382dbbc18a77e3569bfd0
Xuan Tong <txmanialife@gmail.com>
2018-09-27 20:42:03 +1000

    add functionality

--- legit.pl
+++ legit.pl
@@ -11,68 +11,8 @@ my $rootDirName = ".legit";
 my $legitStateCurrentBranchFileName = ".legitStatesCurrentBranch";
 my $branchStateTrackedFilesIndexFileName = ".branchStatesTrackedFilesIndex";
 my $branchStateCommitNumberFileName = ".branchStatesCommitNumber";
+my $branchStateAddedFileFileName = ".branchStatesAddedFiles"; # ad hoc, only exists between add and commit
 my $masterBranchName = "master";
-
-
-###### main switch case
-if (@ARGV == 0) {
-	print $helpMessage;
-	exit 0;
-}
-
-if ($ARGV[0] eq "init") {
-	init();
-} elsif($ARGV[0] eq "add") {
-	add(@ARGV[1..@ARGV-1]);
-} else {
-	print "legit.pl: error: unknown command $ARGV[0]\n".$helpMessage;
-}
-
-###### functions
-sub init {
-	if (-e $rootDirName and -d $rootDirName) {
-		print "$0: error: .legit already exists\n";
-		return;
-	}
-	my $return = mkdir $rootDirName;
-	if ($return != 1) {
-		die "Error creating new folder:".$!."\n";
-	} else {
-		print "Initialized empty legit repository in $rootDirName\n";
-	}
-
-	system "touch $rootDirName/$legitStateCurrentBranchFileName;
-		echo \"master\" > $rootDirName/$legitStateCurrentBranchFileName";
-	initBranch($masterBranchName);
-}
-
-sub add {
-	my @filesToAdd = @_;
-	checkIfRootDirExist();	
-
-	OUTER: foreach my $file (@filesToAdd) {
-		if (-e $file and -f $file) {
-			# check if it already exists in trackFileIndex
-			print "$rootDirName/".getCurrentBranch()."/$branchStateTrackedFilesIndexFileName";
-			open(my $fh, '<', "$rootDirName/".getCurrentBranch()."/$branchStateTrackedFilesIndexFileName");
-			INNER: while(my $line = <$fh>)  {   
-			    if ($line eq $file) {
-			    	print "File $file already added blah blah\n";
-			    	close($fh);
-			    	last OUTER;
-			    }
-			}
-			close($fh);
-			# append to .branchStatesTrackedFilesIndex,append
-			open(my $write_fh, '>>', "$rootDirName/".getCurrentBranch()."/$branchStateTrackedFilesIndexFileName");  
-			print $write_fh "$file\n";
-			close($write_fh);
-		} else {
-			print "$file does not valid blah blah\n";
-		}
-	}
-}
-
 
 
 ###### helper functions
@@ -107,5 +47,82 @@ sub getCurrentBranch {
 
 # }
 
+
+
+###### functions
+sub init {
+	if (-e $rootDirName and -d $rootDirName) {
+		print "$0: error: .legit already exists\n";
+		return;
+	}
+	my $return = mkdir $rootDirName;
+	if ($return != 1) {
+		die "Error creating new folder:".$!."\n";
+	} else {
+		print "Initialized empty legit repository in $rootDirName\n";
+	}
+
+	system "touch $rootDirName/$legitStateCurrentBranchFileName;
+		echo \"master\" > $rootDirName/$legitStateCurrentBranchFileName";
+	initBranch($masterBranchName);
+}
+
+sub add {
+	my @filesToAdd = @_;
+	checkIfRootDirExist();	
+
+	# Get already tracked files
+	open(my $fh, '<', "$rootDirName/".getCurrentBranch()."/$branchStateTrackedFilesIndexFileName");
+	chomp(my @trackedFiles = <$fh>);
+	close $fh;
+
+	if (-e "$rootDirName/".getCurrentBranch()."/$branchStateAddedFileFileName") {
+		open (my $fh, '<', "$rootDirName/".getCurrentBranch()."/$branchStateAddedFileFileName");
+		while (my $line = <$fh>) {
+			chomp $line;
+			push @trackedFiles, $line;
+		}
+	} else {
+		system "touch "."$rootDirName/".getCurrentBranch()."/$branchStateAddedFileFileName";
+	}
+	my %trackedFilesHash = map { $_ => 1 } @trackedFiles;
+
+	# print "already exists files: \n";
+	# foreach (sort keys %trackedFilesHash) {
+	# 	print "$_ : $trackedFilesHash{$_}\n";
+	# }
+
+	# prepare write to .branchStatesAddedFile
+	open(my $write_fh, '>>', "$rootDirName/".getCurrentBranch()."/$branchStateAddedFileFileName");
+
+	foreach my $file (@filesToAdd) {
+		if (-e $file and -f $file) {
+			# check if it already exists in trackedFilesHash			
+			if(!exists($trackedFilesHash{$file})) { 
+				# write to `addedFile` 
+				print $write_fh "$file\n";
+			}
+		} else {
+			print "$file does not valid blah blah\n";
+		}
+	}
+
+	close($write_fh);
+}
+
+
+###### main switch case
+if (@ARGV == 0) {
+	print $helpMessage;
+	exit 0;
+}
+
+if ($ARGV[0] eq "init") {
+	init();
+} elsif($ARGV[0] eq "add") {
+	add(@ARGV[1..@ARGV-1]);
+} else {
+	print "legit.pl: error: unknown command $ARGV[0]\n".$helpMessage;
+}
 
 
